69d8180ac3bc399aa28972f05b10d60b241263bb
Andres Canu <andres8m@hotmail.com>
2017-11-13 23:00:00 -0600

    Initial commit

---
+++ pages/home/homeController.js
@@ -0,0 +1,5 @@
+angular.module('scotchApp')
+    .controller('homeController', function ($scope) {
+        // create a message to display in our view
+        $scope.message = 'Everyone come and see how good I look!';
+    });
